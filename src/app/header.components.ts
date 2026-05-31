1a09d2f23095bd56762b11c142fd3f917984502b
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-05-31 17:40:13 +0300

    add header component

---
+++ src/app/header.components.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'header-component',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
